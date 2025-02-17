389d40e3db880e78d6b5ba8d3e742e9790800088
mikel314 <mikel314@gmail.com>
2025-02-17 18:48:00 +0000

    create post_install.sh

---
+++ infra/post_installs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Creado entorno virtual con conda"
+
+# Activar Conda
+source /home/mikel/miniconda3/bin/activate
+
+# Crear un entorno Conda (si no existe)
+conda create --name MLenv python=3.13 -y
+
+# Activar el entorno recién creado
+conda activate MLenv
+
+# Opcional: Instalar paquetes en el entorno
+conda install pip -y
+
+# instalamos los paquetes del proyecto
+pip install -r /home/mikel/devproject/requeriments.txt -y
+
+echo "El entorno mi_entorno ha sido activado."
