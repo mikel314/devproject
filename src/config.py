d0fc873d3cf9135367127f316c7bc490d9d4893a
mikel314 <mikel314@gmail.com>
2025-02-20 20:24:20 +0000

    added read_gsheet.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# path to the home directory
+path_home = "/home/mikel/"
+
+# path to repository
+path_repo = path_home + "devproject/"
+
+# path do the data directory
+path_data = path_repo + "data/"
+
+# file with credentials
+cred_file = path_home + "mikelinv-1fa650952440.json"
---
+++ src/read_gsheet.py
@@ -0,0 +1,33 @@
+import gspread
+import pandas as pd
+import config as cf
+
+
+def read_gsheet(credentials, filename, sheetname):
+    # Autenticación con Google Sheets
+    gc = gspread.service_account(filename=credentials)
+
+    # Abrir el documento de Google Sheets por nombre
+    spreadsheet = gc.open(filename)
+
+    # Seleccionar la pestaña (tab) por nombre
+    worksheet = spreadsheet.worksheet(sheetname)
+
+    # Obtener todos los valores de la hoja
+    data = worksheet.get_all_records()
+
+    # Convertir a DataFrame de Pandas
+    df = pd.DataFrame(data)
+
+    return df
+
+
+def save_df(df, path, filename):
+    df.to_parquet(path + filename + ".parquet")
+    # df = pd.read_parquet("data.parquet", engine="pyarrow")
+
+
+# Read master data from Google Sheets
+tracker_master = read_gsheet(cf.cred_file, "Tracker", "Master")
+print(tracker_master.head)
+tracker_master.to_parquet(cf.path_data + "tracker_master.parquet")
